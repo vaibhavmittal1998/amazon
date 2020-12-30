16545e99c97f13269a587b92d1f0b2681dd23f85
vaibhavmittal1998 <vaibhav.mittal1998@gmail.com>
2020-12-30 12:40:09 +0530

    add services nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home';
+import ServiceNavbar from './Components/ServiceNavbar/ServiceNavbar';
 
 function App() {
   return (
     <div className="app">
 
       {/* Header Component */}
-      <Header/>    
+      <Header/>  
+      <ServiceNavbar />   
 
       {/* Home Component */}
       <Home/>
