b9beffcf421cd0325c42fb17ca4c24acaf106c17
vaibhavmittal1998 <vaibhav.mittal1998@gmail.com>
2021-01-30 21:50:46 +0530

    add product on home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Product from "./../Product/Product";
 
 function Home() {
   var settings = {
@@ -10,12 +11,12 @@ function Home() {
     fade: true,
     autoplay: true,
     autoplaySpeed: 3000,
+    arrows: false,
   };
 
   return (
     <div className="home">
       <div className="home__container">
-          
         <Slider {...settings}>
           <div>
             <img
@@ -53,22 +54,64 @@ function Home() {
             ></img>
           </div>
         </Slider>
+        <div className="home__row__container">
+          <div className="home__row">
+            <Product
+              title="URBN 10000 mAh Li-Polymer Ultra Compact Power Bank with 12W Fast Charge (Blue)"
+              price="599.00"
+              image="https://m.media-amazon.com/images/I/31e-bvE+q5L._AC_SR400,600_.jpg"
+              rating={3}
+            />
+            <Product
+              title="Havells Vitonica Juicer Mixer Grinder with 3 Stainless Steel Jar (White and Blue)"
+              price="3,599.00"
+              image="https://images-eu.ssl-images-amazon.com/images/I/41V3xvQL12L._AC_US160_FMwebp_QL70_.jpg"
+              rating={4}
+            />
+            <Product
+              title="Vivo V19 (Mystic Silver, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers"
+              price="24,999.00"
+              image="https://m.media-amazon.com/images/I/41Ph1PiqtVL.__AC_SY400_.jpg"
+              rating={4}
+            />
+          </div>
 
-        <div className="home__row">
-            {/* <Product /> */}
-            {/* <Product /> */}
+          <div className="home__row">
+            <Product
+              title="Samsung LC49HG90DMUXEN 48.9-inch Ultra Wide Curved Monitor (Black)"
+              price="1,53,900.00"
+              image="https://images-na.ssl-images-amazon.com/images/I/81vlA84pg6L._SL1500_.jpg"
+              rating={4}
+            />
+          </div>
+
+          <div className="home__row">
+            <Product
+              title="VSK Bean Bag XXXL Sofa Mudda Cover Black (Without Beans) Cover only"
+              price="980.00"
+              image="https://m.media-amazon.com/images/I/51QO4iATxCL._AC_SY400_.jpg"
+              rating={4}
+            />
+            <Product
+              title="Redux RWS0218S Analogue Blue Dial Men's and Boys' Watch"
+              price="1679.00"
+              image="https://m.media-amazon.com/images/I/51898Ck3WCL._AC_SR400,600_.jpg"
+              rating={4}
+            />
+            <Product
+              title="Amazon Brand - Symbol Men Sweatshirt"
+              price="800.00"
+              image="https://m.media-amazon.com/images/I/31gXMVdurfL._AC_SR400,600_.jpg"
+              rating={4}
+            />
+            <Product
+              title="Redux RWS0272S Analogue IPG Golden Dial Men’s and Boy's Watch"
+              price="3000.00"
+              image="https://m.media-amazon.com/images/I/41BC5qXlbaL._AC_SR400,600_.jpg"
+              rating={4}
+            />
+          </div>
         </div>
-
-        <div className="home__row">
-            {/* <Product /> */}
-            {/* <Product /> */}
-        </div>
-
-        <div className="home__row">
-            {/* <Product /> */}
-            {/* <Product /> */}
-        </div>
-
       </div>
     </div>
   );
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Product.css"
 
-function Product() {
+function Product({title, price, image, rating}) {
     return (
-        <div>
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <div className="product__price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </div>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐️</p>
+                    ))}
+                </div>
+            </div>
+
+            <img src={image} alt=""></img>
+
+            <button>Add to Basket</button>
             
         </div>
     )
