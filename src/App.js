230b7de70c347446d2d5864c8666cc19d23fdf39
vaibhavmittal1998 <vaibhav.mittal1998@gmail.com>
2021-01-30 22:42:51 +0530

    add routing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
-import Header from './Components/Header/Header'
-import Home from './Components/Home/Home';
-import ServiceNavbar from './Components/ServiceNavbar/ServiceNavbar';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import ServiceNavbar from "./Components/ServiceNavbar/ServiceNavbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout/Checkout";
 
 function App() {
   return (
-    <div className="app">
-
-      {/* Header Component */}
-      <Header/>  
-      <ServiceNavbar />   
-
-      {/* Home Component */}
-      <Home/>
-      
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <ServiceNavbar />
+        <Switch>
+          <Route path="/checkout">
+            <h1>Hello World</h1>
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/ServiceNavbar/ServiceNavbar.js
+++ src/Components/ServiceNavbar/ServiceNavbar.js
@@ -9,24 +9,24 @@ function ServiceNavbar() {
                 <RoomOutlinedIcon />
                 <span className="service_navbar_option_pair">
                     <span className="service_navbar_option_address_key"> Your </span>
-                    <span className="service_navbar_option_address_value"> Address </span>
+                    <span className="service_navbar_option_address_value"> <strong>Address</strong> </span>
                 </span>
             </div>
 
             <div className="service__navbar__option__services">
-                <span className="service_navbar_option_service_key"> Gift Cards </span>
-                <span className="service_navbar_option_service_key"> Best Sellers </span>
-                <span className="service_navbar_option_service_key"> Mobiles </span>
-                <span className="service_navbar_option_service_key"> Today's Deal </span>
-                <span className="service_navbar_option_service_key"> New Release </span>
-                <span className="service_navbar_option_service_key"> Customer Service </span>
-                <span className="service_navbar_option_service_key"> Fashion </span>
-                <span className="service_navbar_option_service_key"> Amazon Pay </span>
+                <span className="service_navbar_option_service_key"> <strong>Gift Cards</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Best Sellers</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Mobiles</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Today's Deal</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>New Release</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Customer Service</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Fashion</strong> </span>
+                <span className="service_navbar_option_service_key"> <strong>Amazon Pay</strong> </span>
             </div>
 
             <div className="service__navbar__option__quote">
                 <img src="https://img.icons8.com/bubbles/2x/amazon.png" className="amazon_quote_img"/>
-                <span className="service_navbar_option_quote_value"> Shopping made easy | Download the app </span>
+                <span className="service_navbar_option_quote_value"> <strong>Shopping made easy | Download the app</strong> </span>
             </div>
         </div>
     );
